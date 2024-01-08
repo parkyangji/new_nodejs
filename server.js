5254a5e1d473566b5e65438b28ee4c933dbed417
parkyangji <dhsfid02@gmail.com>
2024-01-08 12:42:18 +0900

    db연동

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const app = express();
 // 폴더안에 파일들을 html에서 사용가능
 app.use(express.static(__dirname + '/public'));
 
+const { MongoClient } = require('mongodb');
+
+let db;
+const url = 'mongodb+srv://parkyangji:rhdandnjs02@cluster0.2n908hd.mongodb.net/?retryWrites=true&w=majority';
+new MongoClient(url).connect().then((client) => {
+  console.log('DB연결성공');
+  db = client.db('board');
+}).catch((err)=>{
+  console.log(err);
+})
+
 app.listen(8080, ()=>{
   console.log('8080에서 서버 실행중')
 });
