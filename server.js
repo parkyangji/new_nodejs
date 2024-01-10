894de9b1ebcecbf0a6ee2754091e9a32dec3d231
parkyangji <dhsfid02@gmail.com>
2024-01-10 10:09:33 +0900

    method-override 추가

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ObjectId } = require('mongodb');
+const methodeOverride = require('method-override');
 
 // 폴더안에 파일들을 html에서 사용가능
 app.use(express.static(__dirname + '/public'));
 //ejs 템플릿 연결
 app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
+// 폼 태그에서 put, delete 요청 등 맘대로
+app.use(methodeOverride('_method'));
 
-const {
-  MongoClient,
-  ObjectId
-} = require('mongodb');
+
 
 let db;
 const url = 'mongodb+srv://parkyangji:rhdandnjs02@cluster0.2n908hd.mongodb.net/?retryWrites=true&w=majority';
@@ -52,10 +51,12 @@ app.post('/add', async (req, res) => {
     if (req.body.title === '') {
       res.send('제목입력안했는데?')
     } else {
+
       await db.collection('post').insertOne({
         title: req.body.title,
         content: req.body.content
       });
+
       res.redirect('/list'); // 유저를 다른 페이지로 이동시킴
     }
   } catch (e) {
@@ -67,13 +68,9 @@ app.post('/add', async (req, res) => {
 app.get('/detail/:id', async (req, res) => {
 
   try {
-    let result = await db.collection('post').findOne({
-      _id: new ObjectId(req.params.id)
-    })
+    let result = await db.collection('post').findOne({_id: new ObjectId(req.params.id)});
     if (result === null) res.status(400).send('null');
-    res.render('detail.ejs', {
-      정보: result
-    });
+    res.render('detail.ejs', { 정보: result });
   } catch (e) {
     //console.log(e)
     res.status(400).send('이상한 url 입력함');
@@ -81,25 +78,21 @@ app.get('/detail/:id', async (req, res) => {
 });
 
 app.get('/edit/:id', async (req, res) => {
-  let result = await db.collection('post').findOne({
-    _id: new ObjectId(req.params.id)
-  })
-  res.render('edit.ejs', {
-    정보: result
-  });
+  let result = await db.collection('post').findOne({_id: new ObjectId(req.params.id)});
+  res.render('edit.ejs', { 정보: result });
 });
 
-app.post('/edit', async (req, res) => {
-  await db.collection('post').updateOne({
-    _id: new ObjectId(req.body.id)
+app.put('/edit', async (req, res) => {
+
+  await db.collection('post').updateOne({ // updateMany도 있음
+    _id: new ObjectId(req.body.id) // 필터링 코드 $gt, $gte, $ne ...도 있음
   }, {
-    $set: {
+    $set: { // $inc (기존값에 +/-) $mul (기존값에 x) $unset (필드값 삭제)
       title: req.body.title,
       content: req.body.content
     }
   })
+
   let result = await db.collection('post').find().toArray(); // db 내용을 가져오기
-  res.render('list.ejs', {
-    글목록: result
-  })
+  res.render('list.ejs', { 글목록: result });
 });
